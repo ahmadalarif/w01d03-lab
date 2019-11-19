6d6cf24b77b95b16cbf9d35e0246699d80aa8185
ahmadalarif <44748381+ahmadalarif@users.noreply.github.com>
2019-11-19 19:00:24 +0300

    Update sara order.js

--- sara order.js
+++ sara order.js
@@ -1,7 +1,42 @@
+//#### Exercise 1:
+
 function Alphabetsort(str){
     var x = str.split("");
     return x.sort().join("");
 }
-console.log(Alphabetsort("aras"))
+console.log(Alphabetsort("hello"))
+
+//#### Exercise 2:
+
+function reversed(num) {
+    var a = num.toString().split('').reverse().join('');
+    return a;
+  }
+  var result =reversed(987654321);
+  console.log(result);
+
+  //#### Exercise 3:
 
 
+  function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 37; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+  
+      alert( i ); // a prime
+    }
+  }
+  //#### Exercise 4:
+
+  function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 37; j < i; j++) {
+        if (i % j > 1) continue nextPrime;
+      }
+  
+      alert( i ); // a prime
+    }
+  }
